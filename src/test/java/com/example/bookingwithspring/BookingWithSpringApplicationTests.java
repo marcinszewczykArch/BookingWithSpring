8eee9793a615f937f19e90d9c1aedbfcf3d252cb
Marcin Szewczyk <marcinszewczyk.arch@gmail.com>
2022-01-24 19:37:41 +0100

    test

--- src/test/java/com/example/bookingwithspring/BookingWithSpringApplicationTests.java
+++ src/test/java/com/example/bookingwithspring/BookingWithSpringApplicationTests.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ContextConfiguration(classes = BookingWithSpringApplicationTests.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class BookingWithSpringApplicationTests {
